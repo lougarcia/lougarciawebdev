65bcff9bc1426b739de2873d82890233e91ea504
Lou Garcia <bizlougarcia@gmail.com>
2025-08-12 12:38:42 +0800

    anchor fix for projects section

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -2,11 +2,11 @@ export const header = [
     // { href: "/", text: "Home" },
     { href: "/#about", text: "About" },
     { href: "/#experience", text: "Experience" },
-    { href: "/#projects", text: "Projects" },
+    { href: "/#projects-section", text: "Projects" },
     { href: "/blogs", text: "Blogs", links: [
         { href: "/blogs/news/", text: "News" },
         { href: "/blogs/snippets/", text: "Snippets" },
-        { href: "/blogs/bookmarks/", text: "Bookmarts" }, // website urls
+        { href: "/blogs/bookmarks/", text: "Bookmarks" }, // website urls
         { href: "/blogs/tutorials/", text: "Tutorials" },
     ] },
 ];
