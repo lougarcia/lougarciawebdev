75c10c27ae01171bcdd6884e4aa3491cd47038b6
Lou Garcia <bizlougarcia@gmail.com>
2025-08-12 08:30:46 +0800

    ermoved server components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
-import cloudflare from '@astrojs/cloudflare';
-
-const isCloudflare = process.env.CF_PAGES === 'true';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +9,4 @@ export default defineConfig({
         plugins: [tailwindcss()],
     },
     integrations: [mdx()],
-    output: 'server', // Enable SSR
-    adapter: isCloudflare
-        ? cloudflare()
-        : node({ mode: 'standalone' })
 });
